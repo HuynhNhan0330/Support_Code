023a70de8f5ccbb8420bf2666ef1f74a8a44a659
HuynhNhan <huynhnhandanang@gmail.com>
2022-10-15 19:45:09 +0700

    Update DSU

--- Disjoint_set_union.cpp
+++ Disjoint_set_union.cpp
@@ -4,23 +4,49 @@ using namespace std;
 
 const int xmax = 1e5 + 5;
 
-int n, parent[xmax];
+int n, parent[xmax], sz[xmax], ranh[xmax];
 
 void makeSet()
 {
-    for (int i = 0; i < xmax; ++i) parent[i] = i;
+    for (int i = 0; i < xmax; ++i) 
+    {
+        parent[i] = i;
+        ranh[i] = 0; // Gốc của cây có độ cao = 0
+        sz[i] = 1; // Ban đầu tập hợp có kích cỡ = 1
+    }
 }
 
 int findSet(int u)
 {
     while (u != parent[u]) u = parent[u];
     return u;
+
+    // nén đường đi
+    return u == parent[u] ? u : parent[u] = findSet(parent[u]);
 }
 
 void unionSet(int u, int v)
 {
     int up = parent[u], vp = parent[v];
-    parent[up] = vp;
+
+    if (up != vp) {
+        /* ============== Xét theo độ cao ===============
+
+        if (ranh[up] < ranh[vp]) swap(up, vp); // quy ước up luôn là gốc
+        parent[up] = vp;
+        if (ranh[up] == ranh[vp]) ++ranh[up];
+
+        ================================================================*/
+        
+        /* ================= Xét theo kích thước ============== 
+        if (sz[up] < sz[vp]) swap(up, vp); // quy ước up luôn là gốc
+        parent[up] = vp;
+        sz[up] += sz[vp];
+        
+        =================================================================*/
+ 
+        parent[up] = vp; // cơ bản
+    }
 }
 
 int32_t main() 

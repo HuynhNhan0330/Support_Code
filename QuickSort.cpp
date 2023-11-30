8aab9ccc467283cd2fb2bba93f0a61ce417495b8
Huỳnh Mai Cao Nhân <huynhnhandanang@gmail.com>
2023-11-30 20:21:42 +0700

    update quick sort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -9,11 +9,11 @@ int partition(int arr[], int low, int high) {
     for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
-            std::swap(arr[i], arr[j]);
+            swap(arr[i], arr[j]);
         }
     }
 
-    std::swap(arr[i + 1], arr[high]);
+    swap(arr[i + 1], arr[high]);
     return i + 1;
 }
 
